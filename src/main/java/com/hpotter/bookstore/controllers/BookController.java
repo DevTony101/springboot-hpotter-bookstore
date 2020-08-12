a46b459844bf1b623cacfef303ca507de315588e
DevTony101 <tonycortesano@gmail.com>
2020-08-12 15:48:27 -0500

    Adding base url

--- src/main/java/com/hpotter/bookstore/controllers/BookController.java
+++ src/main/java/com/hpotter/bookstore/controllers/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/")
 public class BookController {
 
     private final BookRepository repository;
