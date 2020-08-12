d182e8c7781ffdf295ed82f435599da2a8363e07
DevTony101 <tonycortesano@gmail.com>
2020-08-12 16:02:25 -0500

    Adding repository annotation

--- src/main/java/com/hpotter/bookstore/repositories/BookRepository.java
+++ src/main/java/com/hpotter/bookstore/repositories/BookRepository.java
@@ -2,6 +2,8 @@ package com.hpotter.bookstore.repositories;
 
 import com.hpotter.bookstore.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
