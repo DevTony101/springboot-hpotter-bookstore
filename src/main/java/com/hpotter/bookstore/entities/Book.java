d7e6ce42a7966e9e4f7e3b4da61616a007e57dbe
DevTony101 <tonycortesano@gmail.com>
2020-08-12 15:35:15 -0500

    Added BEAN class for a single Book

---
+++ src/main/java/com/hpotter/bookstore/entities/Book.java
@@ -0,0 +1,31 @@
+package com.hpotter.bookstore.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@Table(name = "book")
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "quantity", nullable = false)
+    private String quantity;
+
+    @Column(name = "price", nullable = false)
+    private String price;
+
+    @Column(name = "cover", nullable = false)
+    private String cover;
+}
